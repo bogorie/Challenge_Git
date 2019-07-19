c143976a7204aa2836b497a9071fedeff496f6a2
Rie Kaneko <rkaneko@endava.com>
2019-07-19 11:58:21 -0500

    Added mult funtion

--- code.cpp
+++ code.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
 using namespace std;
 
+int mult(int a, int b){
+    return a*b;
+}
+
 int main() {
     cout << "Hello, World!";
+    int a = 3;
+    int b = 2;
+    cout << a << "x" << b  << "= " << mult(a,b) << endl;
     return 0;
 }
