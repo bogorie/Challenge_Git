6737e8821641572619f2149de0ec78ba40cd3702
Rie Kaneko <rkaneko@endava.com>
2019-07-19 11:53:47 -0500

    added sum funtion

--- code.cpp
+++ code.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
 using namespace std;
 
+int sum(int a, int b){
+    return a+b;
+}
+
 int main() {
     cout << "Hello, World!";
+    int a = 3;
+    int b = 2;
+    cout << a << "+" << b  << "= " << sum(a,b) << endl;
     return 0;
 }
